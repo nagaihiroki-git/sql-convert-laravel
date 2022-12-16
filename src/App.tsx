9331a15061e610f13f00d264c5c97e55fe864447
Hiroki Nagai <82458324+nagaihiroki-git@users.noreply.github.com>
2022-12-16 22:41:27 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import pluralize from 'pluralize';
+
 function App() {
     const [sql, setSql] = useState('');
     const [result, setResult] = useState('');
@@ -20,11 +20,7 @@ function App() {
         const tableName = sql.split('`')[1];
         const columns = sql.substring(sql.indexOf(tableName) + tableName.length).split('`').filter((v, i) => !v.includes(',') && !v.includes(')') && !v.includes('(') && v.length > 0);
         const values = sql.substring(sql.indexOf('VALUES') + 6).split('),').map(v => v.replace('(', '').replace(')', '').replace(';', '').split(',').map(v => v.trim()));
-        const res=values.map((va,i)=>{
-            const row=(plural2Singular(snake2UpperCamel(tableName))+'::create(['+va.map((v,i)=>columns[i]+"=>"+v)+']);');
-            return row;
-        }).join('\n');
-        setResult(res);
+        setResult(values.map((va,i)=>(plural2Singular(snake2UpperCamel(tableName)) + '::create([' + va.map((v, i) => columns[i] + "=>" + v) + ']);')).join('\n'));
     }
     const texts = result.split("\n").map((item, index) => {
         return (
